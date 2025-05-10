94e9fefbc78f4e8694455383dfcc71feb6a670a3
Rizwan ullah <50233953+rizwanmahi@users.noreply.github.com>
2025-05-10 13:08:19 +0500

    Add files via upload

---
+++ pipe_status.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+printf "\n"
+cat <<EOF
+
+
+
+EOF
+
+# Create a new screen session
+echo -e "\n📟 Resuming Pipe Point Status"
+    cd ~/pipe-node
+    while true; do
+        echo '📊 Node Status:'
+        ./pop --status
+        echo ''
+        echo '🏆 Check Points:'
+        ./pop --points
+        echo ''
+        echo '⏰ Check Active Time:'
+        ./pop --stats
+        echo '🔄 Updating in 10 seconds...'
+        sleep 10
+    done
